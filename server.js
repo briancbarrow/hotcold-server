ed0b1eacaf128ac27ea41d1f86b5ba721bbc2c14
Brian Barrow <briancbarrow@gmail.com>
2016-12-13 19:32:38 -0700

    fixed port

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ app.put('/fewest-guesses', function(req, res) {
     })
   })
 })
-app.listen(process.env.port || port, function() {
+app.listen(process.env.PORT || port, function() {
   console.log("listening on port 3000")
 })
